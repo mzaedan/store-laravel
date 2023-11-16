65e62a9dccfe325d4bb6a7f121a49d494c327cd2
mzaedan <zaedanm108@gmail.com>
2023-11-16 12:21:35 +0700

    update

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\TransactionDetail;
+use App\Models\User;
+
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('pages.dashboard');
+        $transaction = TransactionDetail::with(['transaction.user', 'product.galleries'])
+            ->whereHas('product', function($product){
+                $product->where('users_id', Auth::user()->id);
+            });
+
+        $revenue = $transaction->get()->reduce(function ($carry, $item){
+            return $carry + $item->price;
+        });
+
+        $customer = User::count();
+
+        return view('pages.dashboard',[
+            'transaction_count' => $transaction->count(),
+            'transaction_data' => $transaction->get(),
+            'revenue' => $revenue,
+            'customer' => $customer
+        ]);
     }
 }
--- resources/views/pages/cart.blade.php
+++ resources/views/pages/cart.blade.php
@@ -42,6 +42,9 @@
                   </tr>
                 </thead>
                 <tbody>
+                  @php
+                      $totalPrice = 0;
+                  @endphp
                   @foreach ($carts as $cart)
                     <tr>
                       <td style="width: 20%">
@@ -69,6 +72,9 @@
                         </form>
                       </td>
                   </tr>
+                  @php
+                      $totalPrice += $cart->product->price 
+                  @endphp
                   @endforeach
                 </tbody>
               </table>
@@ -83,56 +89,58 @@
               <h2 class="mb-4">Shipping Details</h2>
             </div>
           </div>
-
-          <div class="row mb-2" data-aos="fade-up" data-aos-delay="200">
+          <form action="" id="locations">
+            <div class="row mb-2" data-aos="fade-up" data-aos-delay="200">
             <div class="col-md-6">
               <div class="form-group">
-                <label for="addressOne">Address 1</label>
+                <label for="address_one">Address 1</label>
                 <input
                   type="text"
                   class="form-control"
-                  id="addressOne"
-                  name="addressOne"
+                  id="address_one"
+                  name="address_one"
                   value="Setra Duta Cemara"
                 />
               </div>
             </div>
             <div class="col-md-6">
               <div class="form-group">
-                <label for="addressTwo">Address 2</label>
+                <label for="address_two">Address 2</label>
                 <input
                   type="text"
                   class="form-control"
-                  id="addressTwo"
-                  name="addressTwo"
+                  id="address_two"
+                  name="address_two"
                   value="Blok B2 No.34"
                 />
               </div>
             </div>
             <div class="col-md-4">
               <div class="form-group">
-                <label for="province">Province</label>
-                <select name="province" id="province" class="form-control">
-                  <option value="West Java">West Java</option>
+                <label for="provinces_id">Province</label>
+                <select name="provinces_id" id="provinces_id" class="form-control" v-if="provinces" v-model="provinces_id">
+                  <option v-for="province in provinces" :value="province.id">@{{ province.name }}</option>
                 </select>
+                <select v-else class="form-control"></select>
               </div>
             </div>
             <div class="col-md-4">
               <div class="form-group">
-                <label for="city">City</label>
-                <select name="city" id="city" class="form-control">
-                  <option value="Bandung">Bandung</option>
+                <label for="regencies_id">City</label>
+                <select name="regencies_id" id="regencies_id" class="form-control" v-if="regencies" v-model="regencies_id">
+                  <option v-for="regency in regencies" :value="regency.id">@{{ regency.name }}</option>
                 </select>
+                <select v-else class="form-control"></select>
               </div>
             </div>
             <div class="col-md-4">
               <div class="form-group">
-                <label for="postalCode">Postal Code</label>
+                <label for="zipcode">Postal Code</label>
                 <input
                   type="text"
                   class="form-control"
-                  id="postalCode"
-                  name="postalCode"
+                  id="zipcode"
+                  name="zipcode"
                   value="40554"
                 />
               </div>
@@ -151,12 +159,12 @@
             </div>
             <div class="col-md-6">
               <div class="form-group">
-                <label for="mobile">Mobile</label>
+                <label for="phone_number">Mobile</label>
                 <input
                   type="text"
                   class="form-control"
-                  id="mobile"
-                  name="mobile"
+                  id="phone_number"
+                  name="phone_number"
                   value="+628 2020 11111"
                 />
               </div>
@@ -174,19 +182,19 @@
 
           <div class="row" data-aos="fade-up" data-aos-delay="200">
             <div class="col-4 col-md-2">
-              <div class="product-title">$10</div>
+              <div class="product-title">$0</div>
               <div class="product-subtitle">Country Tax</div>
             </div>
             <div class="col-4 col-md-2">
-              <div class="product-title">$280</div>
+              <div class="product-title">$0</div>
               <div class="product-subtitle">Product Insurance</div>
             </div>
             <div class="col-4 col-md-2">
-              <div class="product-title">$580</div>
+              <div class="product-title">$0</div>
               <div class="product-subtitle">Ship to Jakarta</div>
             </div>
             <div class="col-4 col-md-2">
-              <div class="product-title text-success">$392,409</div>
+              <div class="product-title text-success">{{ number_format($totalPrice ?? 0) }}</div>
               <div class="product-subtitle">Total</div>
             </div>
             <div class="col-8 col-md-3">
@@ -195,7 +203,54 @@
               >
             </div>
           </div>
+          </form>
         </div>
       </section>
     </div>
 @endsection
+
+@push('addon-script')
+
+<script src="{{ url('/vendor/vue/vue.js') }}"></script>
+<script src="https://unpkg.com/axios@1.1.2/dist/axios.min.js"></script>
+  <script>
+    var locations = new Vue({
+      el: "#locations",
+      mounted() {
+        AOS.init();
+        this.getProvincesData();
+      },
+      data: {
+        provinces: null,
+        regencies: null,
+        provinces_id : null,
+        regencies_id : null,
+      },
+      methods:{
+        getProvincesData() {
+          var self = this;
+          axios.get('{{ route('api-provinces') }}')
+            .then(function(response){
+              self.provinces = response.data;
+            })
+
+        },
+        getRegenciesData(){
+          var self = this;
+          axios.get('{{ url('api/regencies') }}/' + self.provinces_id)
+            .then(function(response){
+              self.regencies = response.data;
+            })
+        }
+      },
+      watch: {
+        provinces_id: function(val, oldVal) {
+          this.regencies_id = null;
+          this.getRegenciesData();
+        }
+      }
+    }); 
+
+  </script>
+
+@endpush
